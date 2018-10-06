449493d2edb29239ec812a3efff2ab2f3bc7bc49
1337menohax <vivu493@gmail.com>
2018-10-06 14:12:16 -0700

    Still need to add limit

--- app/src/main/java/com/example/mbpsdownloadcalculator/MainActivity.java
+++ app/src/main/java/com/example/mbpsdownloadcalculator/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mbpsdownloadcalculator;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,42 +14,81 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText et_mbps, et_mib;
     TextView tv_result;
-    Button calculate;
+
 
     double transferTime = 0;
-    int mbps = 0;
+    int userMbps,userMib = 0;
+    boolean flag_mbps,flag_mib = false;
 
-    private Handler handler = new Handler();
-//TODO find a better way to update textview when type in editview
-    //fast clock for checking
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            //tv_result.setText(" "+mbps);
-
-            //handler.removeCallbacks(runnable);
-            //handler.postDelayed(this,500);
-        }
-    };
+    private double calculate(int mbps, int mib){
+        double megaBit = mib * 8.389;
+        return (megaBit/mbps);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        handler.postDelayed(runnable,500);
         tv_result =  findViewById(R.id.resultText);
         et_mbps =  findViewById(R.id.mbpsText);
         et_mib = findViewById(R.id.mibText);
-        calculate = findViewById(R.id.button);
 
-        //String temp_mbps = tv_mbps.getText().toString();
-        //mbps = Integer.parseInt(temp_mbps);
-
-        calculate.setOnClickListener(new View.OnClickListener() {
+        et_mbps.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                //mbps = Integer.parseInt(et_mbps.getText().toString());
-                tv_result.setText(" "+mbps);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!et_mbps.getText().toString().isEmpty()){
+
+                    userMbps = Integer.parseInt(et_mbps.getText().toString());
+                    flag_mbps = true;
+                }
+
+                //TODO limit charsequence
+                /*if(charSequence.length() >= 3){
+                    et_mbps.setText("123");
+                }
+                Log.i("HIIII", String.valueOf(charSequence.length()));*/
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(flag_mbps && flag_mib){
+                    transferTime = calculate(userMbps,userMib);
+                    tv_result.setText(String.format("%.1f",transferTime));
+
+                }
+            }
+        });
+        et_mib.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!et_mib.getText().toString().isEmpty()){
+                    userMib = Integer.parseInt(et_mib.getText().toString());
+                    if(userMib == 0 ){
+                        tv_result.setText("Invalid file size");
+                        flag_mib = false;
+                    }
+                    else{
+                        flag_mib = true;
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(flag_mbps && flag_mib){
+                    transferTime = calculate(userMbps,userMib);
+                    tv_result.setText(String.format("%.1f",transferTime));
+
+                }
             }
         });
     }
