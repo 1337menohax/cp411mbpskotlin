a7d5da0ed862be05c0722c958fcc2975a464f12c
1337menohax <vivu493@gmail.com>
2018-10-16 16:02:57 -0700

    add comment

--- app/src/main/java/com/example/mbpsdownloadcalculator/MainActivity.kt
+++ app/src/main/java/com/example/mbpsdownloadcalculator/MainActivity.kt
@@ -8,37 +8,34 @@ import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
-    internal val et_mbps: EditText
-    internal val et_mib: EditText
-    internal val tv_result: TextView
 
 
-    internal val transferTime = 0.0
-    internal val userMbps: Int = 0
+
+    /*internal val transferTime = 0.0
     internal val userMib = 0
     internal val flagMbps: Boolean = false
-    internal val flagMib = false
+    internal val flagMib = false*/
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_result = findViewById(R.id.resultText)
+        /*tv_result = findViewById(R.id.resultText)
         et_mbps = findViewById(R.id.mbpsText)
-        et_mib = findViewById(R.id.mibText)
+        et_mib = findViewById(R.id.mibText)*/
 
-        et_mbps.addTextChangedListener(object : TextWatcher {
+        mbpsText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
 
             }
 
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-                if (!et_mbps.text.toString().isEmpty()) {
-                    userMbps = Integer.parseInt(et_mbps.text.toString())
-                    flagMbps = true
+                if (!mbpsText.text.toString().isEmpty()) {
+                    val userMbps = Integer.parseInt(mbpsText.text.toString())
+                    val flagMbps = true
                 } else {
-                    tv_result.text = getString(R.string.default_Sec)
-                    flagMbps = false
+                    resultText.text = getString(R.string.default_Sec)
+                    val flagMbps = false
                 }
             }
 
@@ -46,25 +43,25 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(editable: Editable) {
                 if (flagMbps && flagMib) {
                     transferTime = calculate(userMbps, userMib)
-                    tv_result.setText(String.format("%.1f", transferTime))
+                    resultText.setText(String.format("%.1f", transferTime))
                 }
             }
         })
-        et_mib.addTextChangedListener(object : TextWatcher {
+        mibText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-                if (!et_mib.text.toString().isEmpty()) {
-                    userMib = Integer.parseInt(et_mib.text.toString())
+                if (!mibText.text.toString().isEmpty()) {
+                    userMib = Integer.parseInt(mibText.text.toString())
                     flagMib = true
                     //For invalid MiB value
                     if (userMib == 0) {
-                        tv_result.text = "Invalid file size"
+                        resultText.text = "Invalid file size"
                         flagMib = false
                     } else {
                         flagMib = true
                     }
                 } else {
-                    tv_result.text = getString(R.string.default_Sec)
+                    resultText.text = getString(R.string.default_Sec)
                     flagMib = false
                 }//When EditText is empty, replace with default second
                 //flag_mbps = false to avoid calculation
@@ -74,7 +71,7 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(editable: Editable) {
                 if (flagMbps && flagMib) {
                     transferTime = calculate(userMbps, userMib)
-                    tv_result.setText(String.format("%.1f", transferTime))
+                    resultText.setText(String.format("%.1f", transferTime))
 
                 }
             }
